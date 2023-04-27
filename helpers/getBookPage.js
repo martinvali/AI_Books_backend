7516b2c070acfea8dd9deb0f7ba7a9de269bbfec
unknown <martinvali12@gmail.com>
2023-04-27 23:23:55 +0300

    Initial ocmmit

---
+++ helpers/getBookPage.js
@@ -0,0 +1,28 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getBookpage(topic, page, curPage = "") {
+  let prompt;
+  if (page === 1) {
+    prompt = `Write the introductory page of a children's book about ${topic}. In terms of length, it should be between 50 and 100 words.`;
+  } else {
+    prompt = `Write the next page of a children's book about ${topic} in 50 to 100 words. Here is the current page: ${curPage}. `;
+  }
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  return completion.data.choices;
+}
+
+module.exports = getBookpage;
---
+++ index.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const getBookpage = require("./helpers/getBookpage");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.post("/page", async (req, res) => {
+  const { topic, page } = req.body;
+  const text = await getBookpage(topic, page);
+
+  return res.json(text);
+});
+
+app.listen(3000, () => console.log("listening on port 3000"));
